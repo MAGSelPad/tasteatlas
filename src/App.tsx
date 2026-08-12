b454402bcb252a21214fe5ef17fd15438c776097
MAGSelPad <150753146+MAGSelPad@users.noreply.github.com>
2026-08-12 19:50:11 +0000

    Resolución de PENDIENTES

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,60 @@
 import './App.css'
 
-import Grid from '@mui/material/Grid2' 
+import Grid from '@mui/material/Grid2'
 import DishTable from './components/DishTable'
 import Student from './components/Student'
 
 // PENDIENTE: Cree la interfaz
 import { type Dish } from './interface/Dish'
-
+import { useEffect, useState } from 'react'
 
 function App() {
 
   let url = "https://raw.githubusercontent.com/aavendan/datos/refs/heads/main/tasteatlas/bestdishes100-2425.json"
-  
+
   // PENDIENTE: Variable de estado y la función de modificación. 
-  
+  const [data, setData] = useState<Dish[]>([])
 
 
   // PENDIENTE: 
   // Realizar una petición asíncrona a la URL. La respuesta es un JSON. 
   // Al recibir la respuesta, actualice la variable de estado.
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(url)
+      const json = await response.json()
+      // Actualizar la variable de estado
+      setData(json)
+    }
+    getData()
+  }, [])
 
-  
 
 
   return (
     <Grid container spacing={5}>
 
-        {/* Student */}
-        <Grid size={{ xs: 12 }}>
+      {/* Student */}
+      <Grid size={{ xs: 12 }}>
 
-          {/* PENDIENTE: Envíe sus datos (apellidos, nombres y paralelo) como props del componente */}
-          <Student></Student>
+        {/* PENDIENTE: Envíe sus datos (apellidos, nombres y paralelo) como props del componente */}
+        <Student
+          apellidos="Miguel Angel"
+          nombres="Galarza Solis"
+          paralelo="102"
+        />
 
-        </Grid>
-        
-        {/* DishTable */}
-        <Grid size={{ xs: 12 }}>
+      </Grid>
 
-          {/* PENDIENTE: Envíe la variable de estado como prop */}
-          <DishTable data={  }></DishTable>
-        
-        </Grid>
-        
-       
+      {/* DishTable */}
+      <Grid size={{ xs: 12 }}>
+
+        {/* PENDIENTE: Envíe la variable de estado como prop */}
+        <DishTable data={data}></DishTable>
+
+      </Grid>
+
+
     </Grid>
   )
 }
